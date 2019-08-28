5ef2422c05a226e163beb487f38bcc07a8841ca2
Scuba-Chris <chrisceder@hotmail.com>
2019-08-28 11:16:28 -0700

    added target weight and js to stats page

--- js/user.js
+++ js/user.js
@@ -5,42 +5,46 @@ var enter = document.getElementById('enter');
 var currentUser = JSON.parse(localStorage.getItem('userId'));
 var userEmail = 'user-' + currentUser.email;
 
-function User(name, email, age, weight) {
-  this.name = name;
-  this.email = email;
-  this.age = age;
-  this.weight = weight;
+function User(name, email, age, currentWeight, targetWeight) {
+    this.name = name;
+    this.email = email;
+    this.age = age;
+    this.currentWeight = currentWeight;
+    this.targetWeight = targetWeight;
 }
 
 function updateUser(e) {
-  e.preventDefault();
-  userData = {
-    name: e.target.name.value,
-    email: e.target.email.value,
-    age: parseInt(e.target.age.value),
-    weight: parseInt(e.target.weight.value)
-  };
-  localStorage.setItem('user-' + userData.email, JSON.stringify(userData));
-  location.href = 'map.html';
+    e.preventDefault();
+    userData = {
+        name: e.target.name.value,
+        email: e.target.email.value,
+        age: parseInt(e.target.age.value),
+        currentWeight: parseInt(e.target.currentWeight.value),
+        targetWeight: parseInt(e.target.targetWeight.value),
+    };
+    localStorage.setItem('user-' + userData.email, JSON.stringify(userData));
+    location.href = 'map.html';
 }
 
 function newUser(e) {
-  e.preventDefault();
-  var name = e.target.name.value;
-  var email = e.target.email.value;
-  var age = parseInt(e.target.age.value);
-  var weight = parseInt(e.target.weight.value);
-  var newUser = new User(name, email, age, weight);
-  localStorage.setItem('user-' + newUser.email, JSON.stringify(newUser));
-  location.href = 'map.html';
+    e.preventDefault();
+    var name = e.target.name.value;
+    var email = e.target.email.value;
+    var age = parseInt(e.target.age.value);
+    var currentWeight = parseInt(e.target.currentWeight.value);
+    var targetWeight = parseInt(e.target.targetWeight.value);
+    var newUser = new User(name, email, age, currentWeight, targetWeight);
+    localStorage.setItem('user-' + newUser.email, JSON.stringify(newUser));
+    location.href = 'map.html';
 }
 
 // Retrieves the user instance based on the entered id and auto fills the form
 function autoFill() {
-  document.getElementsByTagName('input')[0].value = userData.name;
-  document.getElementsByTagName('input')[1].value = userData.email;
-  document.getElementsByTagName('input')[2].value = userData.age;
-  document.getElementsByTagName('input')[3].value = userData.weight;
+    document.getElementsByTagName('input')[0].value = userData.name;
+    document.getElementsByTagName('input')[1].value = userData.email;
+    document.getElementsByTagName('input')[2].value = userData.age;
+    document.getElementsByTagName('input')[3].value = userData.currentWeight;
+    document.getElementsByTagName('input')[4].value = userData.targetWeight;
 }
 
 // Compares email against the localStorage to find an existing user instance
@@ -48,21 +52,19 @@ function autoFill() {
 // If true, invokes autoFill function
 // Else, prints a greeting message to a new user and asks to fill out the form
 if (localStorage.getItem(userEmail)) {
-  var h2 = document.createElement('h2');
-  greeting.appendChild(h2);
-  h2.textContent = 'Welcome back '+ currentUser.name + '! Please check if the below information is up to date!';
-  greeting.appendChild(h2);
-  var userData = JSON.parse(localStorage.getItem(userEmail));
-  autoFill();
-  enter.addEventListener('submit', updateUser);
+    var h2 = document.createElement('h2');
+    greeting.appendChild(h2);
+    h2.textContent = 'Welcome back ' + currentUser.name + '! Please check if the below information is up to date!';
+    greeting.appendChild(h2);
+    var userData = JSON.parse(localStorage.getItem(userEmail));
+    autoFill();
+    enter.addEventListener('submit', updateUser);
+} else {
+    h2 = document.createElement('h2');
+    greeting.appendChild(h2);
+    h2.textContent = 'Hello ' + currentUser.name + '! Please provide the below infomration!';
+    greeting.appendChild(h2);
+    document.getElementsByTagName('input')[0].value = currentUser.name;
+    document.getElementsByTagName('input')[1].value = currentUser.email;
+    enter.addEventListener('submit', newUser);
 }
-else {
-  h2 = document.createElement('h2');
-  greeting.appendChild(h2);
-  h2.textContent = 'Hello ' + currentUser.name + '! Please provide the below infomration!';
-  greeting.appendChild(h2);
-  document.getElementsByTagName('input')[0].value = currentUser.name;
-  document.getElementsByTagName('input')[1].value = currentUser.email;
-  enter.addEventListener('submit', newUser);
-}
-
