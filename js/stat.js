0df6b663f99032071c39f18d4a8e23fa1fab8d3e
jeremiah <jeremiah.kim@hotmail.com>
2019-08-28 14:15:53 -0700

    completed statjs. CSS needs to be updated

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,68 @@
 'use strict';
+var userId = {
+  name: 'Jeremiah',
+  email: 'jeremiah.kim@hotmail.com',
+};
+localStorage.setItem('userId', JSON.stringify(userId));
+
+var user1 = {
+  name: 'Jeremiah',
+  email: 'jeremiah.kim@hotmail.com',
+  age: 65,
+  currentWeight: 200,
+  targetWeight: 150
+};
+
+localStorage.setItem('user-jeremiah.kim@hotmail.com', JSON.stringify(user1));
+
+var act1 = [
+  {
+    type: 'walk',
+    distance: 1,
+    duration: 1,
+    calorieCount: 100
+  },
+  {
+    type: 'run',
+    distance: 1,
+    duration: 1,
+    calorieCount: 100
+  },
+  {
+    type: 'run',
+    distance: 1,
+    duration: 1,
+    calorieCount: 100
+  },
+  {
+    type: 'walk',
+    distance: 1,
+    duration: 1,
+    calorieCount: 100
+  },
+  {
+    type: 'run',
+    distance: 1,
+    duration: 1,
+    calorieCount: 100
+  },
+  {
+    type: 'bike',
+    distance: 1,
+    duration: 1,
+    calorieCount: 100
+  }
+];
+localStorage.setItem('activities-jeremiah.kim@hotmail.com', JSON.stringify(act1));
 
 var userInfo = document.getElementById('user-info'); //create a section in stat.html with id "user-info"
 var userCheck = JSON.parse(localStorage.getItem('userId'));
 var userEmail = 'user-' + userCheck.email;
 var currentUser = JSON.parse(localStorage.getItem(userEmail));
+var labels = ['Walk', 'Run', 'Bike'];
+var dataCalories = [];
+var dataDistance = [];
+
 var h2 = document.createElement('h2');
 userInfo.appendChild(h2);
 h2.textContent = 'Name: ' + currentUser.name;
@@ -15,11 +74,11 @@ h3.textContent = 'age: ' + currentUser.age;
 
 h3 = document.createElement('h3');
 userInfo.appendChild(h3);
-h3.textContent = 'current weight: ' + currentUser.currentWeight;
+h3.textContent = 'current weight: ' + currentUser.currentWeight + 'lbs';
 
 h3 = document.createElement('h3');
 userInfo.appendChild(h3);
-h3.textContent = 'target Weight: ' + currentUser.targetWeight;
+h3.textContent = 'target Weight: ' + currentUser.targetWeight + 'lbs';
 
 h3 = document.createElement('h3');
 userInfo.appendChild(h3);
@@ -27,6 +86,102 @@ h3.textContent = 'calories to be burnt: ' + (currentUser.currentWeight - current
 
 
 
+function generateData() {
+  var userActivties = 'activities-' + userCheck.email;
+  var activities = JSON.parse(localStorage.getItem(userActivties));
+  console.log(activities);
+  var sumWalkCal = 0;
+  var sumRunCal = 0;
+  var sumBikeCal = 0;
+  var sumWalkDis = 0;
+  var sumRunDis = 0;
+  var sumBikeDis = 0;
+
+  for (var i = 0; i < activities.length; i++) {
+    if(activities[i].type === 'walk') {
+      sumWalkCal += activities[i].calorieCount;
+      sumWalkDis += activities[i].distance;
+    }
+    else if (activities[i].type === 'run') {
+      sumRunCal += activities[i].calorieCount;
+      sumRunDis += activities[i].distance;
+    }
+    else if (activities[i].type === 'bike') {
+      sumBikeCal += activities[i].calorieCount;
+      sumBikeDis+= activities[i].distance;
+    }
+  }
+  dataCalories = [sumWalkCal, sumRunCal, sumBikeCal];
+  dataDistance = [sumWalkDis, sumRunDis, sumBikeDis];
+}
+
+generateData();
+
+
+var ctx = document.getElementById('chart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: 'Total Calories Burnt',
+      data: dataCalories,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+
+var ctx = document.getElementById('chart1').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: 'Total Distance Traveled',
+      data: dataDistance,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
 
 
 
@@ -34,15 +189,7 @@ h3.textContent = 'calories to be burnt: ' + (currentUser.currentWeight - current
 
 
 
-
-
-
-
-
-
-
-
-
+/*
 
 
 
@@ -54,9 +201,9 @@ var ctx = document.getElementById('chart').getContext('2d');
 
 var allData = [
 
-    { name: 'WALKING', calories: 42, duration: 60, },
-    { name: 'RUNNING', calories: 130, duration: 30, },
-    { name: 'BIKING', calories: 75, duration: 10, },
+  { name: 'WALKING', calories: 42, duration: 60, },
+  { name: 'RUNNING', calories: 130, duration: 30, },
+  { name: 'BIKING', calories: 75, duration: 10, },
 ];
 
 var labels = [];
@@ -68,10 +215,10 @@ var colors = ['yellow', 'blue', 'pink', 'salmon', 'orange', 'red'];
 
 for (var i = 0; i < allData.length; i++) {
 
-    labels.push(allData[i].name);
-    data1.push(allData[i].calories);
-    data2.push(allData[i].duration);
-    data3.push(Math.floor(allData[i].calories / allData[i].duration));
+  labels.push(allData[i].name);
+  data1.push(allData[i].calories);
+  data2.push(allData[i].duration);
+  data3.push(Math.floor(allData[i].calories / allData[i].duration));
 }
 
 
@@ -81,69 +228,69 @@ function makeChart(data, labels) {
 
 
 
-    var chart = new Chart(ctx, {
+  var chart = new Chart(ctx, {
 
-        type: 'bar',
+    type: 'bar',
 
-        data: {
+    data: {
 
-            labels: labels,
+      labels: labels,
 
-            datasets: [{
+      datasets: [{
 
-                    label: 'CALORIES',
+        label: 'CALORIES',
 
-                    backgroundColor: colors,
+        backgroundColor: colors,
 
-                    borderColor: 'white',
+        borderColor: 'white',
 
-                    data: data1,
-
-
-                },
-
-                {
-
-                    label: 'DURATION',
-
-                    backgroundColor: colors,
-
-                    borderColor: 'white',
-
-                    data: data2,
+        data: data1,
 
 
-                },
+      },
 
-                {
+      {
 
-                    label: 'COMPARISION',
+        label: 'DURATION',
 
-                    backgroundColor: colors,
+        backgroundColor: colors,
 
-                    borderColor: 'white',
+        borderColor: 'white',
 
-                    data: data3,
-
-
-                },
+        data: data2,
 
 
-            ]
+      },
 
-        },
+      {
 
-        options: {
-            legend: {
-                display: true,
-                labels: {
-                    fontColor: 'rgb(255, 99, 132)',
+        label: 'COMPARISION',
 
-                }
-            }
+        backgroundColor: colors,
+
+        borderColor: 'white',
+
+        data: data3,
+
+
+      },
+
+
+      ]
+
+    },
+
+    options: {
+      legend: {
+        display: true,
+        labels: {
+          fontColor: 'rgb(255, 99, 132)',
+
         }
+      }
+    }
 
-    });
+  });
 
 
 }
@@ -152,9 +299,9 @@ var ctx = document.getElementById('chart').getContext('2d');
 
 var allData1 = [
 
-    { name: 'WALKING', calories: 42, duration: 60, },
-    { name: 'RUNNING', calories: 130, duration: 30, },
-    { name: 'BIKING', calories: 75, duration: 10, },
+  { name: 'WALKING', calories: 42, duration: 60, },
+  { name: 'RUNNING', calories: 130, duration: 30, },
+  { name: 'BIKING', calories: 75, duration: 10, },
 ];
 
 var labels2 = [];
@@ -166,10 +313,10 @@ var colors2 = ['yellow', 'blue', 'pink', 'salmon', 'orange', 'red'];
 
 for (var i = 0; i < allData1.length; i++) {
 
-    labels2.push(allData1[i].name);
-    data4.push(allData1[i].calories);
-    data5.push(allData1[i].duration);
-    data6.push(Math.floor(allData1[i].calories / allData1[i].duration));
+  labels2.push(allData1[i].name);
+  data4.push(allData1[i].calories);
+  data5.push(allData1[i].duration);
+  data6.push(Math.floor(allData1[i].calories / allData1[i].duration));
 }
 
 
@@ -179,69 +326,70 @@ function makeChart1(data, labels) {
 
 
 
-    var chart = new Chart1(ctx, {
+  var chart = new Chart1(ctx, {
 
-        type: 'polarArea',
+    type: 'polarArea',
 
-        data: {
+    data: {
 
-            labels: labels2,
+      labels: labels2,
 
-            datasets: [{
+      datasets: [{
 
-                    label: 'CALORIES',
+        label: 'CALORIES',
 
-                    backgroundColor: colors2,
+        backgroundColor: colors2,
 
-                    borderColor: 'white',
+        borderColor: 'white',
 
-                    data: data4,
-
-
-                },
-
-                {
-
-                    label: 'DURATION',
-
-                    backgroundColor: colors2,
-
-                    borderColor: 'white',
-
-                    data: data5,
+        data: data4,
 
 
-                },
+      },
 
-                {
+      {
 
-                    label: 'COMPARISION',
+        label: 'DURATION',
 
-                    backgroundColor: colors2,
+        backgroundColor: colors2,
 
-                    borderColor: 'white',
+        borderColor: 'white',
 
-                    data: data6,
-
-
-                },
+        data: data5,
 
 
-            ]
+      },
 
-        },
+      {
 
-        options: {
-            legend: {
-                display: true,
-                labels: {
-                    fontColor: 'rgb(255, 99, 132)',
+        label: 'COMPARISION',
 
-                }
-            }
+        backgroundColor: colors2,
+
+        borderColor: 'white',
+
+        data: data6,
+
+
+      },
+
+
+      ]
+
+    },
+
+    options: {
+      legend: {
+        display: true,
+        labels: {
+          fontColor: 'rgb(255, 99, 132)',
+
         }
+      }
+    }
 
-    });
+  });
 
 
 }
+*/
