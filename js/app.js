0f0abd01a28798a64f35563ef44be76a38572bba
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-08-28 11:25:31 -0700

    many little fixes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 'use strict';
 
+// TODO: SOC; page specific code should be in the page specific JS file.
 var walk = document.getElementById('walk');
 var run = document.getElementById('run');
 var bike = document.getElementById('bike');
+
 var listOfActivities = [];
 var currentUser = {};
-var excersiceProperties = [
+var exerciseProperties = [
   ['walk', 3.0, 2.5],
   ['run', 11, 6.7],
   ['bike', 9.3, 14]
@@ -21,20 +23,22 @@ function Activity(type) {
 
 Activity.prototype.duration = function() {
   var speed = 0;
-  for (var i = 0; i < excersiceProperties.length; i++) {
-    if (excersiceProperties[i][0] === this.type) {
-      speed = excersiceProperties[i][2];
+  // TODO: better way?
+  for (var i = 0; i < exerciseProperties.length; i++) {
+    if (exerciseProperties[i][0] === this.type) {
+      speed = exerciseProperties[i][2];
     }
   }
   return this.distance / speed;
 };
 
 // Calories calculation test
-Activity.prototype.calorieCount = function() {
+Activity.prototype.calorieCount = function () {
+  // TODO: what does met stand for?  Comment or change variable name.  Should be obvious to user.
   var met = 0;
-  for (var i = 0; i < excersiceProperties.length; i++) {
-    if (excersiceProperties[i][0] === this.type) {
-      met = excersiceProperties[i][1];
+  for (var i = 0; i < exerciseProperties.length; i++) {
+    if (exerciseProperties[i][0] === this.type) {
+      met = exerciseProperties[i][1];
     }
   }
   return met * currentUser.weight / 2.2 * this.duration;
@@ -50,6 +54,7 @@ function newActivity() {
   localStorage.setItem('activities-' + currentUser.email, JSON.stringify(listOfActivities));
 }
 
+// TODO: SOC; page specific code should be in the page specific JS file.
 walk.addEventListener('click', newActivity);
 run.addEventListener('click', newActivity);
 bike.addEventListener('click', newActivity);
--- js/user.js
+++ js/user.js
@@ -1,7 +1,10 @@
 'use strict';
 
+// TODO: SOC; page specific code should be in the page specific JS file.
+// Shared code should be in app.js or other purpose specific JS file.
 var greeting = document.getElementById('greeting');
 var enter = document.getElementById('enter');
+
 var currentUser = JSON.parse(localStorage.getItem('userId'));
 var userEmail = 'user-' + currentUser.email;
 
@@ -12,6 +15,7 @@ function User(name, email, age, weight) {
   this.weight = weight;
 }
 
+// TODO: SOC; event handlers should be on page specific script, then call.
 function updateUser(e) {
   e.preventDefault();
   userData = {
@@ -20,6 +24,7 @@ function updateUser(e) {
     age: parseInt(e.target.age.value),
     weight: parseInt(e.target.weight.value)
   };
+  // TODO: Objects should update local storage with a single method for that specific purpose.
   localStorage.setItem('user-' + userData.email, JSON.stringify(userData));
   location.href = 'map.html';
 }
@@ -31,12 +36,14 @@ function newUser(e) {
   var age = parseInt(e.target.age.value);
   var weight = parseInt(e.target.weight.value);
   var newUser = new User(name, email, age, weight);
+  // TODO: Objects should update local storage with a single method for that specific purpose.
   localStorage.setItem('user-' + newUser.email, JSON.stringify(newUser));
   location.href = 'map.html';
 }
 
 // Retrieves the user instance based on the entered id and auto fills the form
 function autoFill() {
+  // TODO: Works but this is brittle.  For example what is the form is re-ordered?  This code would also have to be changed.  Would future you know to do that?
   document.getElementsByTagName('input')[0].value = userData.name;
   document.getElementsByTagName('input')[1].value = userData.email;
   document.getElementsByTagName('input')[2].value = userData.age;
@@ -50,19 +57,17 @@ function autoFill() {
 if (localStorage.getItem(userEmail)) {
   var h2 = document.createElement('h2');
   greeting.appendChild(h2);
-  h2.textContent = 'Welcome back '+ currentUser.name + '! Please check if the below information is up to date!';
+  h2.textContent = 'Welcome back ' + currentUser.name + '! Please check if the below information is up to date!';
   greeting.appendChild(h2);
   var userData = JSON.parse(localStorage.getItem(userEmail));
   autoFill();
   enter.addEventListener('submit', updateUser);
-}
-else {
+} else {
   h2 = document.createElement('h2');
   greeting.appendChild(h2);
-  h2.textContent = 'Hello ' + currentUser.name + '! Please provide the below infomration!';
+  h2.textContent = 'Hello ' + currentUser.name + '! Please provide the below information!';
   greeting.appendChild(h2);
   document.getElementsByTagName('input')[0].value = currentUser.name;
   document.getElementsByTagName('input')[1].value = currentUser.email;
   enter.addEventListener('submit', newUser);
 }
-
